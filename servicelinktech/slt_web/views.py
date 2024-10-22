9098e0795a989d476af4dce314b9a22bb742bb12
mbillah88 <itcmbillah007@gmail.com>
2024-10-22 20:40:59 +0000

    new 2310

--- servicelinktech/slt_web/views.py
+++ servicelinktech/slt_web/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+def master(request):
+    template = loader.get_template("slt_web/new.html")
+    return HttpResponse(template.render())
+
+def index(request):
+    template = loader.get_template("slt_web/index.html")
+    return HttpResponse(template.render())
