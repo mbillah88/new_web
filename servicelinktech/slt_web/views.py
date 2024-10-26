0462716980de365a376f6670230cf386aabc09c7
mbillah88 <itcmbillah007@gmail.com>
2024-10-26 18:24:06 +0000

    Pending changes exported from your codespace

--- servicelinktech/slt_web/views.py
+++ servicelinktech/slt_web/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
-def master(request):
+def master1(request):
     template = loader.get_template("slt_web/content/slt_dashboard.html")
     return HttpResponse(template.render())
 
+def master(request):
+    template = loader.get_template("slt_web/test.html")
+    return HttpResponse(template.render())
+
 def index(request):
-    template = loader.get_template("slt_web/index.html")
+    template = loader.get_template("slt_web/test.html")
     return HttpResponse(template.render())
